367c7aadad9bd53fc11620e9e8cd7f1c259d6e7e
unknown <kauetmurakami@gmail.com>
2022-05-31 22:33:10 -0300

    feat: add second page

--- lib/app/modules/home/widgets/second_section.dart
+++ lib/app/modules/home/widgets/second_section.dart
@@ -1,17 +1,46 @@
 import 'package:deebx_gh_page/app/modules/home/controller.dart';
+import 'package:deebx_gh_page/core/consts.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SecondSectionWidget extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('${IMAGES}bg_image_2.jpg'), fit: BoxFit.cover)),
       height: Get.height,
       width: Get.width,
-      child: Column(
-        children: [
-          Text('s'),
-        ],
+      child: FractionallySizedBox(
+        heightFactor: 1,
+        widthFactor: 1,
+        child: Stack(
+          children: [
+            Container(
+              height: Get.height,
+              width: Get.width,
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3), // changes position of shadow
+                ),
+              ]),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Text('s'),
+                ],
+              ),
+            ),
+            Positioned.fill(
+                child: Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Image.asset('${IMAGES}example_art.png')))
+          ],
+        ),
       ),
     );
   }
